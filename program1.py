2cd666125cf5aa63f977e214efff8d631e560b8b
saurabh <nsaurabh111@gmail.com>
2024-10-07 13:51:16 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,29 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-
-
-
-
-
-
-
+def isValid(s: str) -> bool:
+   
+    stack = []
+    
+    
+    matching_brackets = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    
+    
+    for char in s:
+        
+        if char in '({[':
+            stack.append(char)
+       
+        elif char in ')}]':
+           
+            if not stack or stack.pop() != matching_brackets[char]:
+                return False
     
 
+    return len(stack) == 0
 
 
-  
-
+print(isValid("()"))       
+print(isValid("()[]{}"))   
+print(isValid("(]"))
