7e5f9aceede19be93523f8a8da2b5070f3f647de
saurabh <nsaurabh111@gmail.com>
2024-10-07 14:13:42 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,24 +1,35 @@
-def romanToInt(s):
-    roman = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
+class Solution(object):
+    def romanToInt(self, s):
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
-    total = 0
-    for i in range(len(s)):
-        current = roman[s[i]]
-        next_value = roman[s[i + 1]] if i + 1 < len(s) else 0
+        total = 0
+        prev_value = 0
 
-        if current < next_value:
-            total -= current
-        else:
-            total += current
+        for char in reversed(s):
+            current_value = roman_values[char]
 
-    return total
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
 
+            prev_value = current_value
 
+        return total
+
+# Example usage:
+solution = Solution()
+
+print(solution.romanToInt("III"))    # Output: 3
+print(solution.romanToInt("IV"))     # Output: 4
+print(solution.romanToInt("IX"))     # Output: 9
+print(solution.romanToInt("LVIII"))  # Output: 58
+print(solution.romanToInt("MCMXCIV")) # Output: 1994
